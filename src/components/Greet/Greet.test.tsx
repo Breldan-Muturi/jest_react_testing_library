d6a91cddc6c5ac5b5ed17d2e59cba50105bacad2
Breldan Muthaka <breldanmuthaka@Breldans-MacBook-Pro.local>
2022-09-20 10:01:07 +0300

    first commit

---
+++ src/components/Greet/Greet.test.tsx
@@ -0,0 +1,16 @@
+//Greet should render the text hello, and if a name is passed into the component It should render hello followed by the name
+
+import { render, screen } from "@testing-library/react";
+import Greet from "./Greet";
+
+test("Greet renders correctly", () => {
+  render(<Greet />);
+  const textElement = screen.getByText("Hello");
+  expect(textElement).toBeInTheDocument();
+});
+
+test("Greet renders with a name", () => {
+  render(<Greet name="Breldan" />);
+  const textElement = screen.getByText("Hello Breldan");
+  expect(textElement).toBeInTheDocument();
+});
